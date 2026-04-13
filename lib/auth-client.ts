c564011a083fd45559a39a7b155f2d3a07acb3c2
rafael.alves <rafael.rha@gmail.com>
2026-04-13 09:51:55 -0300

    fix: revert invalid emailPasswordClient plugin

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,9 +1,8 @@
 import { createAuthClient } from "better-auth/client"
-import { emailOTPClient, emailPasswordClient } from "better-auth/client/plugins"
+import { emailOTPClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
   plugins: [
-    emailPasswordClient(),
     emailOTPClient(),
   ],
 })
